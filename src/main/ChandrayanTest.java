d6cf2b2f95a11e88787b82d857a3ec091de77e43
Administrator <Administrator@SYSTEMA6.DCSRCC.com>
2023-08-02 14:16:42 +0530

    Refactoring TestCase: Removed not needed testChandrayan() test case

--- src/main/ChandrayanTest.java
+++ src/main/ChandrayanTest.java
@@ -3,13 +3,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ChandrayanTest {
-
-    @Test
-    public void testChandrayan(){
-        Chandrayan chandrayan = new Chandrayan();
-        chandrayan.finalDirection();
-
-    }
     @Test
     public void testSetInitialPosition(){
         Chandrayan chandrayan = new Chandrayan(0, 0, 0, 'N');
