170fa052dc222c5c7665373230e0d35ffa385f0b
Administrator <Administrator@SYSTEMA6.DCSRCC.com>
2023-08-02 14:09:35 +0530

    Passed Second TestCase: Passing initial position for Chandrayan

--- src/main/Chandrayan.java
+++ src/main/Chandrayan.java
@@ -1,6 +1,21 @@
 package main;
 
 public class Chandrayan{
+
+    private int x_coord, y_coord, z_coord;
+    private char direction;
+    public Chandrayan(){
+        this.x_coord = 0;
+        this.y_coord = 0;
+        this.z_coord = 0;
+        this.direction = ' ';
+    }
+    public Chandrayan(int x, int y, int z, char initialDirection){
+        this.x_coord = x;
+        this.y_coord = y;
+        this.z_coord = z;
+        this.direction = initialDirection;
+    }
     public void finalDirection(){
         System.out.println('N');
     }
