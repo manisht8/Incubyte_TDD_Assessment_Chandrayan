7f137a734e52d5997974621f62fb73d4e120fc33
Administrator <Administrator@SYSTEMA6.DCSRCC.com>
2023-08-02 14:30:39 +0530

    Refactoring TestCase and Third Failed TestCase: Added object of Chandrayan class, failed to pass commands for direction

--- src/main/ChandrayanTest.java
+++ src/main/ChandrayanTest.java
@@ -1,10 +1,20 @@
 package main;
 import org.junit.Test;
+import org.junit.BeforeClass;
 import static org.junit.Assert.*;
 
 public class ChandrayanTest {
+
+    Chandrayan chandrayan = new Chandrayan(0, 0, 0, 'N');
+
     @Test
     public void testSetInitialPosition(){
         Chandrayan chandrayan = new Chandrayan(0, 0, 0, 'N');
+    }
+
+    @Test
+    public void testPassingDirectionCommands(){
+        char[] commands = new char[] {'f', 'r', 'u', 'b', 'l'};
+        chandrayan.finalDirection(commands);
     }
 }
