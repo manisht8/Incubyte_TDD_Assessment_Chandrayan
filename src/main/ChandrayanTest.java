10516080df5b0b642297e5c78054ccd8f12ca26c
Administrator <Administrator@SYSTEMA6.DCSRCC.com>
2023-08-02 13:41:51 +0530

    First Failed Case: production code's finalDirection() not found

---
+++ src/main/ChandrayanTest.java
@@ -0,0 +1,13 @@
+package main;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ChandrayanTest {
+
+    @Test
+    public void testChandrayan(){
+        Chandrayan chandrayan = new Chandrayan();
+        chandrayan.finalDirection();
+
+    }
+}
